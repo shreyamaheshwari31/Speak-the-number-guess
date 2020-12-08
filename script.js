45c05b82451e4956b01698d0bf66dfb9181cf1d4
shreyamaheshwari31 <59526166+shreyamaheshwari31@users.noreply.github.com>
2020-12-08 23:47:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const msgEl = document.getElementById('msg');
-const randomNum = getRandomNumber();
-console.log("Number :", randomNum);
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition ;
-function getRandomNum() {
-    return Math.floor(Math.random() * 100) + 1;
-}
